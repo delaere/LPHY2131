c6af89db4d4dbb87e5f9f671c030d30942923e2f
Christophe Delaere <christophe.delaere@cern.ch>
2015-11-15 22:14:19 +0100

    Adapt the display of verbatim elements
    use adjustblock + fancy verbatim to be sure that also long lines are
    fully displayed.

--- documentation/createDoc.sh
+++ documentation/createDoc.sh
@@ -3,6 +3,7 @@
 git clone https://github.com/delaere/LPHY2131.wiki.git
 cd LPHY2131.wiki
 for i in *.md; { pandoc -f markdown -t latex --base-header-level=2 $i -o `basename $i .md`.tex; }
+for i in *.tex; { sed -i.bak -e 's/verbatim/myverb/' $i; }
 cp ../LPHY2131_Documentation.tex .
 pdflatex LPHY2131_Documentation.tex
 pdflatex LPHY2131_Documentation.tex
