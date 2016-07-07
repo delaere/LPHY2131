2f8a1c622572b19ae230d9593e4de6612ecda10f
jtoucheque <julien.toucheque@student.uclouvain.be>
2016-07-07 15:30:29 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ i#!/bin/env sh
 #. LPHY2131/install.sh
 
 xmessage -buttons Ok:0 -nearmouse "Downloading sample CMS data files" -timeout 5 &
-
-# to do : cp -r /nfs/user/jdf/Electron_PAT_test_1.root
+mkdir data
+cp -r /nfs/user/jdf/Electron_PAT_test_1.root /data
 
 xmessage -buttons Ok:0 -nearmouse "Installing CMSSW 4.2.8 (used for 2010 data processing)" -timeout 5 &
 cmsrel CMSSW_5_3_32
@@ -19,19 +19,19 @@ cmsenv
 scram b
 cd ../..
 xmessage -buttons Ok:0 -nearmouse "Installing CMSSW 7.1.1 (used to setup the analysis environment)" -timeout 5 &
-export SCRAM_ARCH=slc5_amd64_gcc481
+export SCRAM_ARCH=slc6_amd64_gcc472
 cmsrel CMSSW_7_1_1
 cd CMSSW_7_1_1/src
 cmsenv
 cd ../..
 xmessage -buttons Ok:0 -nearmouse "Installing MadGraph 5" -timeout 5 &
-wget --no-check-certificate -qO- https://launchpad.net/mg5amcnlo/2.0/2.3.0/+download/MG5_aMC_v2.3.3.tar.gz | tar zxv
-cd MG5_aMC_v2_3_3/
+wget --no-check-certificate -qO- https://launchpad.net/mg5amcnlo/2.0/2.4.0/+download/MG5_aMC_v2.4.2.tar.gz | tar zxv
+cd MG5_aMC_v2_4_2/
 echo "install pythia-pgs" | bin/mg5_aMC
 cd ..
-xmessage -buttons Ok:0 -nearmouse "Installing Delphes 3.3.0" -timeout 5 &
-wget -qO- http://cp3.irmp.ucl.ac.be/downloads/Delphes-3.3.0.tar.gz | tar zxv 
-cd Delphes-3.3.0
+xmessage -buttons Ok:0 -nearmouse "Installing Delphes 3.3.2" -timeout 5 &
+wget -qO- http://cp3.irmp.ucl.ac.be/downloads/Delphes-3.3.2.tar.gz | tar zxv 
+cd Delphes-3.3.2
 make -j 4
 make -j 4 display
 cd
