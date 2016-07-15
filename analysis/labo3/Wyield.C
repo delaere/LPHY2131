0c111f12c520d988fe35a591da50d1e12335897e
jtoucheque <julien.toucheque@student.uclouvain.be>
2016-07-15 14:47:38 +0200

    Update Wyield.C

--- analysis/labo3/Wyield.C
+++ analysis/labo3/Wyield.C
@@ -10,7 +10,8 @@ Float_t low = 20;
 Float_t high = 120;
 TString cuts_signalregion  = "nElectrons>=1";
 TString cuts_controlregion = "nElectrons>=1";
-TFile* data_file = TFile::Open("El2010data_flat.root");
+// do not forget to change the Ntuplefiles with the list of Ntuples data files of either electrons or muons
+TFile* data_file = TFile::Open("Ntuplefiles");
 TFile* simu_file = TFile::Open("delpheAnalysisW.root");
 Float_t scale_factor = 1.;
 
