c9363e9bf88454b805e1524ffb0475d5be4fd30f
jtoucheque <julien.toucheque@student.uclouvain.be>
2016-07-20 14:23:52 +0200

    Update prepare.sh

--- analysis/prepare.sh
+++ analysis/prepare.sh
@@ -11,12 +11,13 @@ if [ ! -f ../Delphes-3.3.2/delpheAnalysisW.root ]; then
     echo "Did you complete lab 1?"
     exit
 fi
-#switch the Ntupes files with the name of the list of ntuples files available on the virtual machine
+#switch the "NtupesfilesMuons" with the path of the list of Ntuples files available on the virtual machine
 if [ ! -f ../data/NtuplesfilesMuons ]; then
     echo "NtuplesfilesMuons File not found!"
     echo "This should have been downloaded by the installation script."
     exit
 fi
+#switch the "NtupesfilesElectrons" with the path of the list of Ntuples files available on the virtual machine
 if [ ! -f ../data/NtuplesfilesElectrons ]; then
     echo "NtuplesfilesElectrons File not found!"
     echo "This should have been downloaded by the installation script."
@@ -25,11 +26,11 @@ fi
 
 # create sym links
 ln -s ../Delphes-3.3.2/delpheAnalysisZ.root labo2/
-#insert sym links for your input Ntuples datafiles in the labo2 directory for electrons and muons
+#insert sym links for the input Ntuples datafiles in the labo2 directory for electrons and muons
 ln -s ../data/NtuplesfilesMuons labo2/
 ln -s ../data/NtuplesfilesElectrons labo2/
 ln -s ../Delphes-3.3.2/delpheAnalysisW.root labo3/
-#create sym links for your input Ntuples datafiles in the labo3 directory for electrons and muons
+#create sym links for the input Ntuples datafiles in the labo3 directory for electrons and muons
 ln -s ../data/NtuplesfilesMuons labo3/
 ln -s ../data/NtuplesfilesElectrons labo3/
 
