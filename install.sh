5bcf9828cc2973e1c3242a8eb5512ccec13bdc71
Christophe Delaere <christophe.delaere@cern.ch>
2016-11-01 22:25:03 +0100

    Preparation for 2016.
    - New files to download from cernbox (links expire after 1 year)
    - small fix to the buildfile

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
-#i#!/bin/env sh
+#!/bin/env sh
 
 #Download the virtual machine from http://opendata.cern.ch/VM/CMS#how
 #after booting, change the keyboard layout (if needed)
 #git clone https://github.com/delaere/LPHY2131.git
-#cd LPHY2131
-#. install.sh
+#. LPHY2131/install.sh
+
+xmessage -buttons Ok:0 -nearmouse "Downloading sample CMS data files, MC and ntuples" -timeout 5 &
+mkdir LPHY2131_data
+wget --no-check-certificate https://cernbox.cern.ch/index.php/s/Vk3v8IfnRj1DzyV/download -O LPHY2131_data/ppChargedCurrentFullsim_ntuple.root
+wget --no-check-certificate https://cernbox.cern.ch/index.php/s/UhQBP40YBUwjnft/download -O LPHY2131_data/doubleMu2011_PAT.root
+wget --no-check-certificate https://cernbox.cern.ch/index.php/s/uUDwDNSxrUEDJXc/download -O LPHY2131_data/doubleEl2011_ntuple.root
+wget --no-check-certificate https://cernbox.cern.ch/index.php/s/Wu1G7I78mkHOTZh/download -O LPHY2131_data/doubleMu2011_ntuple.root
+wget --no-check-certificate https://cernbox.cern.ch/index.php/s/epixhaNsCGrRipo/download -O LPHY2131_data/singleEl2011_ntuple.root
+wget --no-check-certificate https://cernbox.cern.ch/index.php/s/rQJTwPcmGqGUZEP/download -O LPHY2131_data/singleMu2011_ntuple.root
 
 xmessage -buttons Ok:0 -nearmouse "Installing CMSSW 5.3.32 (used for 2011 data processing)" -timeout 5 &
 cmsrel CMSSW_5_3_32
@@ -22,8 +30,8 @@ cmsenv
 cd ../..
 
 xmessage -buttons Ok:0 -nearmouse "Installing MadGraph 5" -timeout 5 &
-wget --no-check-certificate -qO- https://launchpad.net/mg5amcnlo/2.0/2.4.0/+download/MG5_aMC_v2.4.2.tar.gz | tar zxv
-cd MG5_aMC_v2_4_2/
+wget --no-check-certificate -qO- https://launchpad.net/mg5amcnlo/2.0/2.4.x/+download/MG5_aMC_v2.4.3.tar.gz | tar zxv
+cd MG5_aMC_v2_4_3/
 echo "install pythia-pgs" | bin/mg5_aMC
 cd ..
 
