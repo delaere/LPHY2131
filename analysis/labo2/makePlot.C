88b03b02d29c536901327ffe28001ef078b21da1
Christophe Delaere <christophe.delaere@cern.ch>
2016-11-28 12:10:49 +0100

    Improved fit of the Z peak
    The fit is first performed on the MC, using the theoretical natural width. That way, we determine the experimental resolution (Gaussian width). This can then be used in the data fit (not done by default).
    The fit is better without constraint, but the two contributions to the width are correlated at 80%. By fixing the experimental resolution, one gets a better estimate of the natural width.

--- analysis/labo2/makePlot.C
+++ analysis/labo2/makePlot.C
@@ -49,23 +49,41 @@ WeakBosonsAnalysis->Draw(Form("%s>>%s_simu",(const char*)var,(const char*)var),c
 hsimu->Scale(scale_factor);
 hsimu->SetFillColor(kYellow);
 
-// fit the data (only makes sense for the Z mass plot)
+// fit function (the fit only makes sense for the Z mass plot)
 TF1 *func = new TF1("myVoigt", myVoigt ,40, 150,6);
-func->SetParameter(0,1e7.0);   func->SetParName(0,"const");
-func->SetParameter(1,90.0);   func->SetParName(1,"mean");
-func->SetParameter(2,1.0);    func->SetParName(2,"sigma");
-func->SetParameter(3,1.0);    func->SetParName(3,"gamma");
- func->SetParameter(4,10);     func->SetParName(4,"BkgConst");
- func->SetParameter(5,0);      func->SetParName(5,"BkgSlope");
- func->SetParLimits(0,10,1e10);
- func->SetParLimits(1,85,95);
- func->SetParLimits(2,0,10);
- func->SetParLimits(3,0,10);
- func->SetParLimits(4,0,1000);
- func->SetParLimits(5,-1,0);
-hdata->Fit("myVoigt","LLR","SAME");
-TF1 *fit = hsimu->GetFunction("myVoigt");
-if(fit) fit->SetLineColor(kRed);
+func->SetParameter(0,1e5.0);   func->SetParName(0,"const");
+func->SetParameter(1,90.0);    func->SetParName(1,"mean");
+func->SetParameter(2,1.0);     func->SetParName(2,"sigma");
+func->SetParameter(3,3.0);     func->SetParName(3,"gamma");
+func->SetParameter(4,0);       func->SetParName(4,"BkgConst");
+func->SetParameter(5,0);       func->SetParName(5,"BkgSlope");
+func->SetParLimits(0,10,1e10);
+func->SetParLimits(1,85,95);
+func->SetParLimits(2,0,10);
+func->SetParLimits(3,0,10);
+func->SetParLimits(4,0,1000);
+func->SetParLimits(5,-1,0);
+
+// fit the MC with fixed BW width
+func->FixParameter(3,2.4952);
+hsimu->Fit("myVoigt","LLR","0");
+TF1 *mcfit = hsimu->GetFunction("myVoigt");
+func->SetParLimits(3,0,10);
+//uncomment the line below to fix the resolution to the 
+//one in simulation.
+//func->FixParameter(2,mcfit->GetParameter(2));
+delete mcfit;
+hdata->Draw();
+hsimu->Draw("HIST SAME");
+
+// fit the data
+TFitResultPtr r = hdata->Fit("myVoigt","LLRS","SAME");
+TF1 *fit = hdata->GetFunction("myVoigt");
+if(fit) {
+   fit->SetLineColor(kRed);
+   fit->SetNpx(1000);
+}
+r->Print("V");
 
 // add a legend
 leg = new TLegend(0.15, 0.68, 0.35,0.88);
