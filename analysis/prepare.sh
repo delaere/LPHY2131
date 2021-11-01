347be263ffbbdbb6a8a0896edb48868465bd34dc
Christophe Delaere <christophe.delaere@cern.ch>
2021-11-01 17:16:13 +0100

    adopt the new name of the course (LPHYS2131)

--- analysis/prepare.sh
+++ analysis/prepare.sh
@@ -12,13 +12,13 @@ if [ ! -f ../../Delphes-3.3.2/delpheAnalysisW.root ]; then
     exit
 fi
 #switch the "NtupesfilesMuons" with the path of the list of Ntuples files available on the virtual machine
-if [ ! -f ../../LPHY2131_data/doubleMu2011_ntuple.root ]; then
+if [ ! -f ../../LPHYS2131_data/doubleMu2011_ntuple.root ]; then
     echo "doubleMu2011_ntuple.root File not found!"
     echo "This should have been downloaded by the installation script."
     exit
 fi
 #switch the "NtupesfilesElectrons" with the path of the list of Ntuples files available on the virtual machine
-if [ ! -f ../../LPHY2131_data/doubleEl2011_ntuple.root ]; then
+if [ ! -f ../../LPHYS2131_data/doubleEl2011_ntuple.root ]; then
     echo "doubleEl2011_ntuple.root File not found!"
     echo "This should have been downloaded by the installation script."
     exit
@@ -26,12 +26,12 @@ fi
 
 # create sym links
 ln -sf ../../../Delphes-3.3.2/delpheAnalysisZ.root labo2/
-ln -sf ../../../LPHY2131_data/doubleMu2011_ntuple.root labo2/
-ln -sf ../../../LPHY2131_data/doubleEl2011_ntuple.root labo2/
-ln -sf ../../../LPHY2131_data/ppNeutralCurrentFullsim_ntuple.root labo2/
+ln -sf ../../../LPHYS2131_data/doubleMu2011_ntuple.root labo2/
+ln -sf ../../../LPHYS2131_data/doubleEl2011_ntuple.root labo2/
+ln -sf ../../../LPHYS2131_data/ppNeutralCurrentFullsim_ntuple.root labo2/
 ln -sf ../../../Delphes-3.3.2/delpheAnalysisW.root labo3/
-ln -sf ../../../LPHY2131_data/singleMu2011_ntuple.root labo3/
-ln -sf ../../../LPHY2131_data/singleEl2011_ntuple.root labo3/
-ln -sf ../../../LPHY2131_data/ppChargedCurrentFullsim_ntuple.root labo3/
+ln -sf ../../../LPHYS2131_data/singleMu2011_ntuple.root labo3/
+ln -sf ../../../LPHYS2131_data/singleEl2011_ntuple.root labo3/
+ln -sf ../../../LPHYS2131_data/ppChargedCurrentFullsim_ntuple.root labo3/
 
 echo "Analysis workspace ready"
